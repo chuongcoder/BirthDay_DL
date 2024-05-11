521afdcd6cc4c07f0f92b391961348832f42ddb4
chuongdev <chuongminh3225@gmail.com>
2024-05-12 00:31:48 +0700

    edit

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Truy cập thẻ audio
-    var audio = document.getElementById("noelAudio");
+    var audio = document.getElementById("birthDayaudio");
 
     // Bắt đầu chơi nhạc khi trang web tải xong
     audio.play();
 });
 document.addEventListener("click", function() {
-    var audio = document.getElementById("noelAudio");
+    var audio = document.getElementById("birthDayaudio");
     audio.play();
   });
